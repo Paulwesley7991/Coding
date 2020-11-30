2c94e665d9d7153cd3a5d6742c0097a4ac42b1c4
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-11-30 16:56:44 +0100

    C++ 1 Intro
    Simple C++ codes

---
+++ C++/C++  1/projects/Project_ein/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ string x= "";
+cout << " please enter your name " <<endl;
+    cin >> x;
+    cout << "Hello "<<x<< endl;
+    return 0;
+}
