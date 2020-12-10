959af6ac7fb764711063c42a35317eab46f46ba0
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-10 05:55:16 +0100

    Update Staff Holiday Mangement
    created date class

--- C++/C++  1/projects/School_work/Staff Holiday Management.cpp
+++ C++/C++  1/projects/School_work/Staff Holiday Management.cpp
@@ -3,6 +3,43 @@
 #include <cstdlib>
 
 using namespace std;
+
+class Date{
+private:
+    short day;
+    short month;
+    unsigned int year;
+public:
+    void enterDate();
+    void showData();
+};
+void Date::enterDate()
+{
+    cout << "Day: ";
+    cin >> day;
+    cout << "Month: ";
+    cin >> month;
+    cout << "Year: ";
+    cin >> year;
+}
+
+void Date::showData()
+{
+    cout << day << "." << month << "." << year << endl;
+}
+
+class Employee
+{
+private:
+    string Surname;
+    string Firstname;
+    string Middlename;
+    Date Dob;
+
+public:
+    Employee(){}
+
+};
 int main()
 {cout << "Testing Program" << endl;
 system("pause");
