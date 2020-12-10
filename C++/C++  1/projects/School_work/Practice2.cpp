add436df4a0cac3bb62255490f7bddce05e09382
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-10 07:49:12 +0100

    Error handling with files
    write protected, file does not exist

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -1,46 +1,33 @@
-#include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 using namespace std;
-// DoSomething throws different types, depending on the
-// value of the Problem parameter.
-void DoSomething(int Problem)
-{
- switch (Problem)
- {
- case 0: throw 5; break; // throws int
- case 1: throw (string)"test.dat"; break; // throws string
- case 2: throw 2.1; break; // throws double
- case 3: throw 'c'; break; // throws char
- }
-}
-// Test program
 int main()
 {
- // insert a number for the Problem variable
- int Selection;
- cout << "Insert a number between 0 and 3:" << endl;
- cin >> Selection;
- // The try block catches the exception in DoSomething
+ ifstream output_data;
+ ofstream copy_data;
  try
  {
- DoSomething (Selection);
+ output_data.open("source.txt",ios::in);
+ copy_data.open("backupcopy.txt");
+ if(!output_data.good())
+ throw (string)"source.txt"; // If the file does not exist
+ else if(!copy_data.good())
+ throw (string)"backupcopy.txt"; // If the file is write-protected
+
+ char ch;
+ while(output_data.get(ch)){ // Read character by character from the file
+ copy_data.put(ch); // Copy character by character into the file
  }
- catch(int i) // Handler for int
+ }
+ catch(string filename)
  {
- cerr << "Integer " << i << endl;
- }
- catch(string s) // Handler for string
- {
- cerr << "String " << s << endl;
- }
- catch(double f) // Handler for double
- {
- cerr << "Float " << f << endl;
- }
- catch(...) // Handles all the other types of exceptions
- {
- cerr << "General Error" << endl;
- }
+ cerr << "Error: " << filename << " cannot be opened." << endl; // cerr is used like cout
+ } // Output as an error
+
+ output_data.close();
+ copy_data.close();
+
  system("pause");
  return 0;
 }
