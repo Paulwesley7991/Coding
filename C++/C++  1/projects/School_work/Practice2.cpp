0dafee8677447c71d1d9f53ca26c2bfcf605e987
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-10 07:47:24 +0100

    practice of Error handling
    Try, catch and throw

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -1,19 +1,46 @@
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+// DoSomething throws different types, depending on the
+// value of the Problem parameter.
+void DoSomething(int Problem)
+{
+ switch (Problem)
+ {
+ case 0: throw 5; break; // throws int
+ case 1: throw (string)"test.dat"; break; // throws string
+ case 2: throw 2.1; break; // throws double
+ case 3: throw 'c'; break; // throws char
+ }
+}
+// Test program
 int main()
 {
- try // Instruction where an error could occur
+ // insert a number for the Problem variable
+ int Selection;
+ cout << "Insert a number between 0 and 3:" << endl;
+ cin >> Selection;
+ // The try block catches the exception in DoSomething
+ try
  {
- int divisor = 0;
- int dividend = 1;
- int quotient = dividend/divisor; // An error occurs, the program would crash!
+ DoSomething (Selection);
  }
- catch (...) // This generally intercepts the error and throws an error message.
+ catch(int i) // Handler for int
  {
- cerr << "Problem detected…"<< endl; // Output of the error using cerr
+ cerr << "Integer " << i << endl;
  }
-
+ catch(string s) // Handler for string
+ {
+ cerr << "String " << s << endl;
+ }
+ catch(double f) // Handler for double
+ {
+ cerr << "Float " << f << endl;
+ }
+ catch(...) // Handles all the other types of exceptions
+ {
+ cerr << "General Error" << endl;
+ }
  system("pause");
  return 0;
 }
