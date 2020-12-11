f9e40a1bde5fb8e934b5245edda26af869fbed3c
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-11 09:50:05 +0100

    Pointer with class
    Pointer to class

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+class Birthday
+{
+ private:
+ int year, month, day;
+ public:
+ Birthday(int j, int m, int t): year(j), month(m), day(t) {} // Constructor with member initializer list
+ Birthday() {} // Default constructor
+
+ void setYear (int j) { year = j; }
+ void setMonth(int m) { month = m; }
+ void setDay(int t) { day = t; }
+ void displayData() { cout << day << "-" << month << "-" << year << endl; }
+};
 int main()
 {
- int x=10, y=10, *xptr, **ptrptr; // Declaration of variables x and y, of pointer xptr, and of the pointer to a pointer ptrptr
+ Birthday Heinz;
+ Birthday *Peter;
 
- cout << "x = " << x << endl; // Output of x as value
+ Heinz.setYear(1964);
+ Heinz.setMonth(1);
+ Heinz.setDay(22);
 
- xptr = &x; // Pointer xptr is assigned the address of x as value
- *xptr = 20; // The variable which xptr points to is assigned value 20
- cout << "x = " << x << " and *xptr = " << *xptr <<endl;
-
- ptrptr = &xptr; // Pointer ptrptr is assigned the address of pointer xptr
- **ptrptr = 30; // Variable x is assigned value 30 via pointer **ptrptr
- cout << "x = " << x << " *xptr = "<< *xptr << " and **ptrptr = " << **ptrptr << endl;
-
- *ptrptr = &y; // The pointer now points to y
- **ptrptr = 40; // Variable y is assigned value 40, since ptrptr points to ptr and ptr is now set to y
- cout << "x = " << x << ", y= " << y << ", *xptr = " << *xptr << " and **ptrptr = " << **ptrptr << endl;
+ Peter = &Heinz;
+ Peter->setMonth(12);
+ Heinz.displayData();
+ Peter->displayData();
  system("pause");
 }
