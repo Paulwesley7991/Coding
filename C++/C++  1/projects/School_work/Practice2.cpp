328a8b5f61d6af57104edd581996d2b7605257ac
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-11 10:03:52 +0100

    Working with pointers
    Practice with pointers

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -1,31 +1,52 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 using namespace std;
-class Birthday
-{
- private:
- int year, month, day;
- public:
- Birthday(int j, int m, int t): year(j), month(m), day(t) {} // Constructor with member initializer list
- Birthday() {} // Default constructor
-
- void setYear (int j) { year = j; }
- void setMonth(int m) { month = m; }
- void setDay(int t) { day = t; }
- void displayData() { cout << day << "-" << month << "-" << year << endl; }
-};
 int main()
 {
- Birthday Heinz;
- Birthday *Peter;
-
- Heinz.setYear(1964);
- Heinz.setMonth(1);
- Heinz.setDay(22);
-
- Peter = &Heinz;
- Peter->setMonth(12);
- Heinz.displayData();
- Peter->displayData();
+ int x = 3;
+ int *pointer = &x;
+ cout << "Address which pointer points to: " << pointer << endl;
+ cout << "Address of x: " << &x << endl;
+ cout << "Address of pointer: " << &pointer << endl << endl;
+ cout << "Value of x: " << x << endl;
+ cout << "Value of pointer: " << *pointer << endl << endl;
+ cout << "Enter value for x: ";
+ cin >> x;
+ cout << endl;
+ cout << "Value of x: " << x << endl;
+ cout << "Value of pointer: " << *pointer << endl << endl;
+ cout << "Enter value for pointer: ";
+ cin >> *pointer;
+ cout << endl;
+ cout << "Value of x: " << x << endl;
+ cout << "Value of pointer: " << *pointer << endl << endl;
  system("pause");
+ int array[5] = {1,2,3,4,5};
+ cout << "Address of the first array element: " << array << endl;
+ cout << "Address of the first array element: " << &array << endl;
+ cout << "Address of the first array element: " << &array[0] << endl;
+ cout << endl;
+ cout << "Value of the first array element: " << array[0] << endl;
+ cout << "Value of the first array element: " << *array << endl << endl;
+ cout << endl;
+ cout << "Value of the third pointer array: " << array[2] << endl;
+ //cout << "Value of the third pointer array: " << *arraypointer[2]; // Does not work!
+ cout << "Value of the third pointer array: " << *(array+2) << endl;
+ cout << "Display address of the array: "<<endl;
+ for(int i = 0; i < 5; ++i)
+ {
+ cout << "&array[" << i << "] = " << &array[i] << endl;
+ }
+ cout << endl;
+ cout << "Display value of the array: "<<endl;
+ for(int i = 0; i < 5; ++i)
+ {
+ cout << "array[" << i << "] = " << array[i] << endl;
+ }
+ system("pause");
+ // SUMMARY:
+ // &array[0] is equal to array
+ // *array is equal to array[0]
+ // &array[1] is equal to array+1
+ // *(arraypointer+1) is equal to array[1]
 }
