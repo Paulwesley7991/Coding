08c06c8517b386140056cdb9d5c3a62ab72ba4de
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-11 06:00:48 +0100

    Pointer to pointer
    pointer to pointer

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 int main()
 {
-int array[]={5,10,15,20,25};
-cout << "The address of the first array is: " << array << endl;
-// or
-cout << "The address of the first array is: " << &array[0] << endl;
-cout << "The value of the first array element is: " << *array << endl;
-// or
-cout << " The value of the first array element is: " << array[0] << endl;
-system("pause");
+ int x=10, y=10, *xptr, **ptrptr; // Declaration of variables x and y, of pointer xptr, and of the pointer to a pointer ptrptr
+
+ cout << "x = " << x << endl; // Output of x as value
+
+ xptr = &x; // Pointer xptr is assigned the address of x as value
+ *xptr = 20; // The variable which xptr points to is assigned value 20
+ cout << "x = " << x << " and *xptr = " << *xptr <<endl;
+
+ ptrptr = &xptr; // Pointer ptrptr is assigned the address of pointer xptr
+ **ptrptr = 30; // Variable x is assigned value 30 via pointer **ptrptr
+ cout << "x = " << x << " *xptr = "<< *xptr << " and **ptrptr = " << **ptrptr << endl;
+
+ *ptrptr = &y; // The pointer now points to y
+ **ptrptr = 40; // Variable y is assigned value 40, since ptrptr points to ptr and ptr is now set to y
+ cout << "x = " << x << ", y= " << y << ", *xptr = " << *xptr << " and **ptrptr = " << **ptrptr << endl;
+ system("pause");
 }
