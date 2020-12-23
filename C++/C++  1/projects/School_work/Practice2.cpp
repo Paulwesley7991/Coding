00768f597e2454c9092442491280e8da1d09b5f3
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-23 17:59:52 +0100

    Operator Overloading
    Overloading cout << and cin >>

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -3,23 +3,36 @@
 #include <ctime>
 using namespace std;
 
-class exam{
-    friend class test;
-public:
-    int x = 8;};
-class test: public exam
-{ friend class exam;
-    public:
-    int f =9;
-    test(){
-    cout<<"worked like magic"<<endl;}
+class Numbers {
+ private:
+ float number1, number2;
+ public:
+ Numbers(): number1(0), number2(0) {}
+ Numbers(float n1, float n2): number1(n1), number2(n2) {}
+
+ // Declaring input/output functions as friend of the class
+ friend istream& operator>> (istream& is, Numbers& a);
+ friend ostream& operator<< (ostream& os, const Numbers& b);
 };
-int main(void){
-exam Exam[2];
-exam EXAM;
-Exam[0] = EXAM;
-cout<< Exam[0].x;
-test Test;
-Exam[1] = Test;
-cout<< Exam[1].f;
-system("pause"); }
+// Input
+istream& operator>> (istream& is, Numbers& a) {
+ cout << endl << "Number1: ";
+ is >> a.number1;
+ cout << "Number2: ";
+ is >> a.number2;
+ return is;
+}
+// Output
+ostream& operator<< (ostream& os, const Numbers& a) {
+ os << "Number1; " << a.number1 << " Number2: " << a.number2 << endl;
+ return os;
+}
+int main()
+{
+ Numbers x;
+ cin >> x;
+ cout << x;
+
+ system("pause");
+ return 0;
+}
