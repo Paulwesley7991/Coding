dadff9e7e60e4e3fc7d55db2f5cf0c669907d967
IkennaPaul97 <75263399+IkennaPaul97@users.noreply.github.com>
2020-12-26 04:57:35 +0100

    Operator Overloading
    Operator Overloading with [ ]

--- C++/C++  1/projects/School_work/Practice2.cpp
+++ C++/C++  1/projects/School_work/Practice2.cpp
@@ -3,6 +3,9 @@
 #include <ctime>
 using namespace std;
 
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 class Numbers {
  private:
  float number1, number2;
@@ -10,49 +13,35 @@ class Numbers {
  Numbers(): number1(0), number2(0) {}
  Numbers(float n1, float n2): number1(n1), number2(n2) {}
 
- //member functions
- const Numbers operator += (const Numbers& a);
- const void operator -= (const Numbers& a);
+ // Prefix increment
+ Numbers& operator++() {
+ number1++;
+ number2++;
+ return *this;
+ }
+
+ // Postfix increment
+ const Numbers operator++ (int) {
+ Numbers t = *this;
+ ++(*this);
+ return t;
+ }
 
  friend ostream& operator<< (ostream& os, const Numbers& b);
- friend Numbers operator+ (const Numbers& left, const Numbers& right);
- friend void operator- (Numbers& left, const Numbers& right);
 };
-const Numbers Numbers::operator += (const Numbers& a) {
- number1 += a.number1;
- number2 += a.number2;
- return *this;
-}
-const void Numbers::operator -= (const Numbers& a) {
- number1 -= a.number1;
- number2 -= a.number2;
-}
 // Output
 ostream& operator<< (ostream& os, const Numbers& a) {
  os << "Number1; " << a.number1 << " Number2: " << a.number2 << endl;
  return os;
 }
-// external function
-Numbers operator+ (const Numbers& left, const Numbers& right) {
- Numbers z;
- z.number1 = left.number1 + right.number1;
- z.number2 = left.number2 + right.number2;
- return z;
-}
-// Note “left” must not be declared as const!
-void operator- (Numbers& left, const Numbers& right) {
- left.number1 -= right.number1;
- left.number2 -= right.number2;
-}
 int main()
 {
- Numbers x(5,6), y(9,18), z;
- x += y;
- x -= y;
- z = x + y;
- cout << z;
- x - y;
- cout << x;
+ Numbers x(15,6), y(10,18), z;
+ // Postfix
+ cout << x++;
+ // Prefix
+ cout << ++x;
+
  system("pause");
  return 0;
 }
