4104d53344861f06ba2ad3834e07696fe8dce688
Ikenna Paul Abonyi <75263399+IkennaPaul97@users.noreply.github.com>
2021-01-13 16:48:52 +0100

    Creating GUI with Python

--- Python/Knowledge_base/main.py
+++ Python/Knowledge_base/main.py
@@ -1,4 +1,15 @@
-def welcome():
-    print("Welcome to Python")
+import wx
 
-welcome()
+class MyFrame(wx.Frame):
+    def __int__(self, parent, title):
+        super(MyFrame, self).__init__(parent, title=title)
+
+class Myapp(wx.App):
+    def OnInit(self):
+        self.frame = MyFrame(parent = None, title= "Our First WIndow", (600, 500))
+        self.frame.Show()
+
+        return True
+
+app = Myapp()
+app.MainLoop()
